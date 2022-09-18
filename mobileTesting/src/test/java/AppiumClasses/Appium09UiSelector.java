c67eb54198d222f8c539ac4e8653b3ad521e3eb0
Gokhan Agar <gokhanagar1@gmail.com>
2022-09-18 11:01:19 +0300

    V11

---
+++ mobileTesting/src/test/java/AppiumClasses/Appium09UiSelector.java
@@ -0,0 +1,86 @@
+package AppiumClasses;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class Appium09UiSelector {
+
+    @Test
+    public void Task2() throws InterruptedException, MalformedURLException {
+
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\gokha\\IdeaProjects\\mobileTesting\\src\\Apps\\gestureTool.apk");
+
+        //noReset
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+
+        //appium server url
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        Thread.sleep(3000);
+
+        // resourceId ile
+       // driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/addButton\")")).click();
+
+        // className
+     //   driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\")")).click();
+
+        //className text ile beraber
+        //driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\").text(\"Add gesture\")")).click();
+
+        //className() index ile
+      //  driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\").index(0)")).click();
+
+        /*
+        List<WebElement> buttons = driver.findElements
+                (new AppiumBy.ByAndroidUIAutomator
+                        ("UiSelector().className(\"android.widget.Button\")"));
+
+        for(WebElement button : buttons){
+
+            if(button.getText().equals("Test")){
+
+                button.click();
+            }
+        }
+        */
+
+
+       // driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\").textContains(\"load\")")).click();
+
+        //Thread.sleep(2000);
+
+        //driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\").textStartsWith(\"Te\")")).click();
+
+        //UiSelector ile kullanilabilecek methodlar
+
+        System.out.println(driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/addButton\").checkable(false)")).getAttribute("checkable"));
+
+        System.out.println(driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/addButton\").clickable(true)")).getAttribute("clickable"));
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ mobileTesting/src/test/java/AppiumClasses/Appium10.java
@@ -0,0 +1,90 @@
+package AppiumClasses;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium10 {
+
+    @Test
+    public void task1() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\gokha\\IdeaProjects\\mobileTesting\\src\\Apps\\apiDemos.apk");
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        desiredCapabilities.setCapability("appPackage","com.android.chrome");
+        desiredCapabilities.setCapability("appActivity","com.google.android.apps.chrome.Main");
+
+
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+
+        Thread.sleep(4000);
+
+
+        driver.findElement(By.xpath("//android.widget.TextView[@text='API Demos']")).click();
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("UiSelector().className(\"android.widget.TextView\").text(\"API Demos\")")).click();
+
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Preference']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//android.widget.TextView[@text='3. Preference dependencies']")).click();
+
+        WebElement checkbox=driver.findElement(By.id("android:id/checkbox"));
+        WebElement wifiSettings=driver.findElement(By.xpath("//android.widget.TextView[@text='WiFi settings']"));
+
+        Thread.sleep(2000);
+        if (!wifiSettings.isEnabled()){
+            checkbox.click();
+        }
+        wifiSettings.click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("android:id/edit")).sendKeys("test");
+        Thread.sleep(2000);
+        driver.findElement(By.id("android:id/button1")).click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
