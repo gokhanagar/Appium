d7285e609fdc04e3518e30f6f062ce9b0e6e712e
Gokhan Agar <gokhanagar1@gmail.com>
2022-09-19 22:37:23 +0300

    V15

--- mobileTesting/src/test/java/AppiumClasses/AppiumBase.java
+++ mobileTesting/src/test/java/AppiumClasses/AppiumBase.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class AppiumBase {
 
@@ -26,10 +27,12 @@ public class AppiumBase {
         desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, true);
         //appium server url
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         Thread.sleep(3000);
         return driver;
 
 
+        
 
 
 
