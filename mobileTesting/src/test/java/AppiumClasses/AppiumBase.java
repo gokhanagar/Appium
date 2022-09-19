76f5c76e47e14bc0015a175820e85562288d18b0
Gokhan Agar <gokhanagar1@gmail.com>
2022-09-19 22:34:14 +0300

    V14

--- mobileTesting/src/test/java/AppiumClasses/AppiumBase.java
+++ mobileTesting/src/test/java/AppiumClasses/AppiumBase.java
@@ -1,8 +1,39 @@
 package AppiumClasses;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class AppiumBase {
 
 
+    public static AndroidDriver androidDriver() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+        desiredCapabilities.setCapability("appPackage", "com.touchboarder.android.api.demos");
+        desiredCapabilities.setCapability("appActivity", "com.touchboarder.androidapidemos.MainActivity");
+
+        //noReset
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        //appium server url
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        Thread.sleep(3000);
+        return driver;
+
+
+
+
+
+    }
 
 
 
@@ -27,5 +58,6 @@ public class AppiumBase {
 
 
 
-    
+
+
 }
