1f7f3ff45b38efce68dff2db8bae11b63e132a30
Gokhan Agar <gokhanagar1@gmail.com>
2022-10-03 07:16:41 +0300

    V23

--- mobileTesting/src/test/java/AppiumClasses/Appium11.java
+++ mobileTesting/src/test/java/AppiumClasses/Appium11.java
@@ -1,4 +1,91 @@
 package AppiumClasses;
 
-public class Appium11 {
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+public class Appium11 extends AppiumBase{
+
+
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+
+        AndroidDriver driver = androidDriver();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
