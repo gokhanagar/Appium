299d9176e5612e340ef12253b96719bea5346bf0
Gokhan Agar <gokhanagar1@gmail.com>
2022-10-24 07:36:21 +0300

    V29

---
+++ mobileTesting/src/test/java/AppiumClasses/Appium14Scroll.java
@@ -0,0 +1,108 @@
+package AppiumClasses;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+
+import java.net.MalformedURLException;
+
+public class Appium14Scroll extends AppiumBase{
+
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+
+        AndroidDriver driver = androidDriver();
+
+        //bu kod ile cihazin olcusunu almis olduk.
+        Dimension dimension = driver.manage().window().getSize();
+
+        int start_x = dimension.height * 0.8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
