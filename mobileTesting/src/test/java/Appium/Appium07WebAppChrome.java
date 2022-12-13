cd0681d06929445eb2108327fc1ac3a5cbe792a4
Gokhan Agar <gokhanagar1@gmail.com>
2022-12-13 18:21:37 +0300

    V49

--- mobileTesting/src/test/java/Appium/Appium07WebAppChrome.java
+++ mobileTesting/src/test/java/Appium/Appium07WebAppChrome.java
@@ -11,31 +11,34 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Set;
 
-
-public class Appium06NativeChrome {
+public class Appium07WebAppChrome {
 
     @Test
     public void test() throws MalformedURLException, InterruptedException {
-
+        String filePath = System.getProperty("user.dir");
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5556");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        capabilities.setCapability("appPackage", "com.android.chrome");
-        capabilities.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"chrome");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,"60000");
 
+        //chrome driver versiyonunuzu burdan indirebilrisiniz
+        //https://chromedriver.storage.googleapis.com/index.html
+        //sonrasinda driver in absolute path bir alt satirda tanimliyorsunuz
+        capabilities.setCapability("chromedriverExecutable",filePath + "\\src\\driver\\chromedriver.exe");
 
         AndroidDriver<MobileElement> driver =
                 new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         Thread.sleep(9000);
         driver.get("https://www.amazon.com");
 
-        System.out.println(driver.getContext() + "<==== app acildigindaki tur");
+        System.out.println(driver.getContext() + "<==== app acildigindaki durumu");
 
         //burada uygulamamnin hangi turleri oldugunu gormek icin getContextHandles() kullaniyoruz
-        /*
+
         Set<String> butunTurler = driver.getContextHandles();
         for(String tur: butunTurler){
             System.out.println(tur);
@@ -43,28 +46,18 @@ public class Appium06NativeChrome {
                 driver.context(tur);
             }
         }
-         */
 
-        System.out.println(driver.getContext() + "<==== degisimden sonraki tur");
+        System.out.println(driver.getContext() + "<==== app sonraki durumu");
         Thread.sleep(7000);
 
-        MobileElement homeScreenLogo = driver.findElementByXPath("//android.view.View[@content-desc=\"Amazon\"]");
-        Assert.assertTrue(homeScreenLogo.isDisplayed());
-        System.out.println("Ana sayfadayiz");
-        //System.out.println(driver.getCurrentUrl()+ " =====url");
+        MobileElement anaSayfaLogo = driver.findElementByCssSelector("a[id='nav-logo-sprites']");
+        Assert.assertTrue(anaSayfaLogo.isDisplayed());
 
-        Thread.sleep(3000);
-        MobileElement sigInButton = driver.findElementByCssSelector("android.view.View[content-desc=\"Sign in ›\"]");
-        sigInButton.click();
-
-        Thread.sleep(5000);
-        MobileElement welcomeText = driver.findElementByCssSelector("android.view.View[text='Welcome']");
-        Assert.assertEquals(welcomeText.getText(),"Welcome");
-        System.out.println("test bitti");
-
-        Thread.sleep(5000);
-        //close session
-        //driver.closeApp();
+        MobileElement signIn = driver.findElementByCssSelector("a[class='nav-a nav-show-sign-in']");
+        signIn.click();
+        Thread.sleep(2000);
+        MobileElement emailNumber = driver.findElementByCssSelector("label[for='ap_email_login']");
+        Assert.assertTrue(emailNumber.isDisplayed());
 
 
 
@@ -137,6 +130,4 @@ public class Appium06NativeChrome {
 
 
     }
-
-
 }
