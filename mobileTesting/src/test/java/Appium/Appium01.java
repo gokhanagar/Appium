64bc500b84e32bccdfbb63d98fd6498f94d67d9d
Gokhan Agar <gokhanagar1@gmail.com>
2022-12-15 09:06:18 +0300

    V52

--- mobileTesting/src/test/java/Appium/Appium01.java
+++ mobileTesting/src/test/java/Appium/Appium01.java
@@ -20,124 +20,20 @@ public class Appium01 {
         //capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android");
         String filePath = System.getProperty("user.dir");
 
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"11.0");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator-5556");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP,filePath + "\\src\\Apps\\gestureTool.apk");
-        capabilities.setCapability("appPackage","com.davemac327.gesture.tool");
-        capabilities.setCapability("appActivity","com.davemac327.gesture.tool.GestureBuilderActivity");
-        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,"60000");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5556");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, filePath + "\\src\\Apps\\gestureTool.apk");
+        capabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
+        capabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivity");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "60000");
 
         AndroidDriver<MobileElement> driver =
-                new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
