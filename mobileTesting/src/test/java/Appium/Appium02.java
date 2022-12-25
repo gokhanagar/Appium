ea3754c0b35b82ff46001bd858572cb2877ef10f
Gokhan Agar <gokhanagar1@gmail.com>
2022-12-25 20:17:00 +0300

    V62

--- mobileTesting/src/test/java/Appium/Appium02.java
+++ mobileTesting/src/test/java/Appium/Appium02.java
@@ -22,17 +22,17 @@ public class Appium02 {
         //capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android");
         String filePath = System.getProperty("user.dir");
 
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"11.0");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator-5556");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP,filePath + "\\src\\Apps\\gestureTool.apk");
-        capabilities.setCapability("appPackage","com.davemac327.gesture.tool");
-        capabilities.setCapability("appActivity","com.davemac327.gesture.tool.GestureBuilderActivity");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5556");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, filePath + "\\src\\Apps\\gestureTool.apk");
+        capabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
+        capabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivity");
 
 
         AndroidDriver<MobileElement> driver =
-                new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+                new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
         System.out.println("app yuklendi");
         Thread.sleep(3000);
@@ -45,49 +45,6 @@ public class Appium02 {
         MobileElement homeScreentitle = driver.findElementById("android:id/title");
         Assert.assertTrue(homeScreentitle.isDisplayed());
         System.out.println("Ana sayfa acildi");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
