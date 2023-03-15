80dfab6ef6ce63756d654e02feeb82f8a44ccedd
gokhan agar <gokhanagar1@gmail.com>
2023-03-15 12:10:11 +0300

    V110

--- mobileTesting/src/test/java/Deneme.java
+++ mobileTesting/src/test/java/Deneme.java
@@ -14,6 +14,7 @@ public class Deneme {
     PreferenceScreen prefScreen = new PreferenceScreen();
     PreferenceDependenciesScreen preferenceDependenciesScreen = new PreferenceDependenciesScreen();
 
+    
     @Test
     public void deneme() {
 
